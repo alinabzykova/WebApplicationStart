29572228623d10dd2b3277f0d748ce07c973f749
alinabzykova <bzykovaaa@gmail.com>
2026-03-11 18:18:23 +0300

    Выполнила задачу 2

---
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index(double a = 0, double b = 0)
+        {
+            double result = a + b;
+
+            return Content($"{a} + {b} = {result}");
+        }
+    }
+}
