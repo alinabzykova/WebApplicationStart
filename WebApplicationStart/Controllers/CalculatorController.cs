8f0fe9ea10845d3ce54027764c4227928b35dcb2
alinabzykova <bzykovaaa@gmail.com>
2026-03-11 18:34:17 +0300

    Выполнила задачу 3

---
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    [Route("calculator")]
+    public class CalculatorController : Controller
+    {
+        [HttpGet("index/{a?}/{b?}/{op?}")]
+        public string Index(double a = 0, double b = 0, string op = "+")
+        {
+            double result = 0;
+
+            if (op == "+")
+            {
+                result = a + b;
+            }
+            else if (op == "-")
+            {
+                result = a - b;
+            }
+            else if (op == "*")
+            {
+                result = a * b;
+            }
+            else
+            {
+                return "Ошибка: используйте операции +, - или *";
+            }
+
+            return $"{a} {op} {b} = {result}";
+        }
+    }
+}
