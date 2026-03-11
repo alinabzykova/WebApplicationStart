0e13107854cd7e8a42f27046eb50a69ee8fd2296
alinabzykova <bzykovaaa@gmail.com>
2026-03-11 18:10:01 +0300

    Выполнила задачу 1

---
+++ WebApplicationStart/Controllers/StartController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebApplicationStart.Controllers
+{
+    public class StartController : Controller
+    {
+        public IActionResult Hello()
+        {
+            int hour = DateTime.Now.Hour;
+            string greeting;
+
+            if (hour >= 0 && hour <= 5)
+                greeting = "Доброй ночи";
+            else if (hour >= 6 && hour <= 11)
+                greeting = "Доброе утро";
+            else if (hour >= 12 && hour <= 17)
+                greeting = "Добрый день";
+            else
+                greeting = "Добрый вечер";
+
+            return Content(greeting);
+        }
+    }
+}
