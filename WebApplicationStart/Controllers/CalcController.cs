b193a4a5da996570d5d5775e708ceeb1ad8096dd
alinabzykova <bzykovaaa@gmail.com>
2026-03-11 18:38:14 +0300

    Выполнила задание 4

---
+++ WebApplicationStart/Controllers/CalcController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    [Route("calc")]
+    public class CalcController : Controller
+    {
+        [HttpGet("index")]
+        public string Index(double a = 0, double b = 0, string c = "+")
+        {
+            double result = 0;
+
+            if (c == "+")
+            {
+                result = a + b;
+            }
+            else if (c == "-")
+            {
+                result = a - b;
+            }
+            else if (c == "*")
+            {
+                result = a * b;
+            }
+            else if (c == "/")
+            {
+                if (b == 0)
+                    return "Ошибка: деление на ноль невозможно";
+
+                result = a / b;
+            }
+            else
+            {
+                return "Ошибка: используйте операции +, -, * или /";
+            }
+
+            return $"{a} {c} {b} = {result}";
+        }
+    }
+}
